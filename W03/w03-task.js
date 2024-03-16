1129952dc43012ad3c9f49fdacc902e9aadbe67a
Kishvell <144922115+Kishvell@users.noreply.github.com>
2024-03-16 11:59:26 -0300

    16/3/2024

--- W03/w03-task.js
+++ W03/w03-task.js
@@ -3,28 +3,55 @@
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
 
+//Function Declaration
+function greet(name) {
+    return `Hello, ${name}!`;
+  }
 
-/* Function Expression - Subtract Numbers */
+    /* Function Expression - Subtract Numbers */
+  // Function Expression
+  let multiply = function(x, y) {
+    return x * y;
+  };
 
+    /* Arrow Function - Multiply Numbers */
+  // Arrow Function
+  let power = (base, exponent) => {
+    return Math.pow(base, exponent);
+  };
 
-/* Arrow Function - Multiply Numbers */
+    /* Open Function Use - Divide Numbers */
+  // Your Choice (IIFE - Immediately Invoked Function Expression)
+  (function() {
+    console.log('This is an IIFE');
+  })();
 
+    /* Decision Structure */
+  // Selection Structure (Ternary Operator)
+  let checkNumber = num => (num % 2 === 0) ? 'Even' : 'Odd';
 
-/* Open Function Use - Divide Numbers */
-
-
-/* Decision Structure */
-
-
-/* ARRAY METHODS - Functional Programming */
-/* Output Source Array */
-
-/* Output Odds Only Array */
-
-/* Output Evens Only Array */
-
-/* Output Sum of Org. Array */
-
-/* Output Multiplied by 2 Array */
-
-/* Output Sum of Multiplied by 2 Array */
+    /* ARRAY METHODS - Functional Programming */
+    /* Output Source Array */
+  // Array Methods - Functional Programming
+  let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    /* Output Odds Only Array */
+    let oddNumbers = numbersArray.filter(num => num % 2 !== 0);
+    /* Output Evens Only Array */
+    let evenNumbers = numbersArray.filter(num => num % 2 === 0);
+    /* Output Sum of Org. Array */
+    let sumOfArray = numbersArray.reduce((sum, num) => sum + num, 0);
+    /* Output Multiplied by 2 Array */
+    let multipliedNumbers = numbersArray.map(num => num * 2);
+    /* Output Sum of Multiplied by 2 Array */
+    let sumOfMultiplied = numbersArray.map(num => num * 2).reduce((sum, num) => sum + num, 0);
+  
+  // Example usage of the functions and array methods
+  console.log(greet('Alice'));
+  console.log(multiply(5, 3));
+  console.log(power(2, 3));
+  console.log(checkNumber(7));
+  console.log(oddNumbers);
+  console.log(evenNumbers);
+  console.log(sumOfArray);
+  console.log(multipliedNumbers);
+  console.log(sumOfMultiplied);
